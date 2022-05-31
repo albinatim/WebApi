ab5e9cc5ea22da4083e1ec0a64e91679a039a948
albinatim <albinatimerbayeva@gmail.com>
2022-05-31 21:55:28 +0500

    4 задача

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -7,16 +7,30 @@
 --(PriceT), цену закрытия предыдущего дня (PriceT1), величину отличия в % (Divergence). В 
 --случае, если текущая цена меньше предыдущей, то Divergence < 0, в противном случае 
 --Divergence > 0
-select distinct tblNew.PriceDate as DateT , tblNew.PriceAssetId as AssetId, tblNew.ClosePrice as PriceT, tblOld.ClosePrice, tblOld.PriceDate
-from tblClosePrice tblNew
-right join tblClosePrice tblOld on
-tblOld.ClosePrice<tblNew.ClosePrice
-where tblNew.PriceAssetId=tblOld.PriceAssetId 
---having DATEDIFF(DAY,tblOld.PriceDate,tblNew.PriceDate)=min(DATEDIFF(DAY,tblOld.PriceDate,tblNew.PriceDate))
-order by tblNew.PriceAssetId asc, tblNew.PriceDate asc
+--select PriceDate as DateT , PriceAssetId as AssetId, ClosePrice as PriceT, 
+--(select PriceDate from tblClosePrice as tblClose 
+--where DATEDIFF(day,tblClose.PriceDate,tbl1.PriceDate) = (-1)
+--and tbl1.PriceAssetId=tblClose.PriceAssetId
+--)
+----, as PriceT1, as Divergence
+--from tblClosePrice as tbl1
+
+----where 
+--order by PriceAssetId asc, PriceDate asc
+
+--;
+
+select tbl1.PriceDate as DateT, tbl1.PriceAssetId as AssetId, tbl1.ClosePrice as PriceT, tbl2.PriceDate,
+tbl2.ClosePrice as PriceT1, CAST((tbl1.ClosePrice-tbl2.ClosePrice)/tbl2.ClosePrice*100 AS INT) as Divergence
+from tblClosePrice as tbl1
+join tblClosePrice tbl2
+on (tbl1.PriceAssetId=tbl2.PriceAssetId)
+where tbl1.PriceAssetId=tbl2.PriceAssetId
+and DATEDIFF(day,tbl2.PriceDate,tbl1.PriceDate) = 1
+and (tbl2.ClosePrice/tbl1.ClosePrice>1.3 or tbl1.ClosePrice/tbl2.ClosePrice>1.3)
 ;
-
-
 --2) Создать необходимые для выборки из п. 1 индексы (написать скрипт создания 
 --индексов)
 
+
+CREATE INDEX [IX_tblClosePrice_PriceAssetId_PriceDate] ON [dbo].[tblClosePrice] ( PriceAssetId,PriceDate )
